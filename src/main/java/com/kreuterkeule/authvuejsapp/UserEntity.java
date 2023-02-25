e919b9b27ace67dcbf60f268e2d6b54fc8011a1c
kreuterkeule <moritz.siefke@protonmail.com>
2023-02-25 13:34:00 +0100

    Step 3 = User Model ready

---
+++ src/main/java/com/kreuterkeule/authvuejsapp/UserEntity.java
@@ -0,0 +1,31 @@
+package com.kreuterkeule.authvuejsapp;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
+
+@Getter @Setter @ToString
+public class UserEntity {
+
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public static UserEntity of(String firstName, String lastName, String email, String password) {
+        return new UserEntity(null, firstName, lastName, email, password);
+    }
+
+    @PersistenceCreator
+    private UserEntity(Long id, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+}
