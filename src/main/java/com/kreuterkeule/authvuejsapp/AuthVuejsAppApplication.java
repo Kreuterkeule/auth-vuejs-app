822d916128253d63ace5239747f8a6a94a4bbd7f
kreuterkeule <moritz.siefke@protonmail.com>
2023-02-25 13:20:26 +0100

    Initial commit

---
+++ src/main/java/com/kreuterkeule/authvuejsapp/AuthVuejsAppApplication.java
@@ -0,0 +1,14 @@
+package com.kreuterkeule.authvuejsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class AuthVuejsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthVuejsAppApplication.class, args);
+	}
+
+}
